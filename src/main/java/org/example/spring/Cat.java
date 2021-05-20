f8b7d4b2c86b19c370291744acaa150e6d5fb3a3
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-05-20 23:31:16 +0400

    Start learn spring

--- src/main/java/org/example/spring/Cat.java
+++ src/main/java/org/example/spring/Cat.java
@@ -1,6 +1,11 @@
 package org.example.spring;
 
 public class Cat implements Pet{
+
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow-meow");
--- src/main/java/org/example/spring/Test3.java
+++ src/main/java/org/example/spring/Test3.java
@@ -9,6 +9,8 @@ public class Test3 {
                         ("applicationContext.xml");
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
 
         context.close();
     }
